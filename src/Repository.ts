9a1ab00800f0e5b79ff049732f93b84bd3197740
Prem4u <prem.max83@gmail.com>
2018-04-07 16:24:11 +0530

    commiting Repos and its changes

---
+++ src/Repository.ts
@@ -0,0 +1,11 @@
+export class Repository{
+    private name:string;
+    private description:string;
+    private url:string;
+
+
+test():void{
+    console.log("Test Successfull !!");
+}
+    
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import {User} from "./User";
-
+import {Repository} from "./Repository";
 
 let user:User=new User();
 user.test();
+let repo:Repository=new Repository();
+repo.test();
